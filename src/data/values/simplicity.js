24acdd6fe3edf46f070c91e9398c99ca233d16ed
Rajasegar Chandran <rajasegar.c@gmail.com>
2022-11-11 16:12:39 +0530

    chore: bring in data from previous repo

---
+++ src/data/values/simplicity.js
@@ -0,0 +1,18 @@
+module.exports = {
+  videos: [
+    {
+      id: "rI8tNMsozo0",
+      duration: 36,
+      year: 2012,
+      description: "Rails Conf 2012 Keynote: Simplicity Matters by Rich Hickey",
+    },
+  ],
+  articles: [
+    {
+      url: "https://css-tricks.com/simple-boring/",
+      title: "Simple is Boring",
+    },
+  ],
+  demos: [],
+  websites: [],
+};
